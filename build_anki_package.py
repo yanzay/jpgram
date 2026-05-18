de145c3d2055737736870ca07cd6fee4aa72aff0
yanzay <yanzay@users.noreply.github.com>
2026-05-19 01:20:27 +0300

    Release v0.11.3

--- build_anki_package.py
+++ build_anki_package.py
@@ -36,7 +36,7 @@ import subprocess
 import sys
 from pathlib import Path
 
-VERSION = "0.11.1"
+VERSION = "0.11.3"
 DECK_NAME = "Japanese Grammar"
 GRAMMAR_DIR = Path("grammar-strict")
 MEDIA_DIR = Path("media")
